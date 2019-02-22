8f135e846051f5f68ccfb89d386a436a742ae35d
Vidar Aune Westrum <vidar@dolittle.com>
2019-02-22 11:56:10 +0100

    A lot of stuff, sorry for bad commit.

--- Source/Hugo/static/js/dolittle.js
+++ Source/Hugo/static/js/dolittle.js
@@ -1,9 +1,18 @@
-(function ($) {
-//left menu expand/collapse
-$('.list-styled .expand').on('click', function () {
-    $parent = $(this).parent();
-    console.log($parent);
-    $parent.toggleClass('active');
-});
+(function($) {
+    function addAnchorTags() {
+        anchors.options = {
+            visible: 'touch'
+        };
 
+        anchors.add('.doc_content h1, .doc_content h2, .doc_content h3, .doc_content h4, .doc_content h5, .doc_content h6');
+    }
+
+    addAnchorTags();
+
+    //left menu expand/collapse
+    $('.list-styled .expand').on('click', function() {
+        $parent = $(this).parent();
+        console.log($parent);
+        $parent.toggleClass('active');
+    });
 })(jQuery);
