28d8a8f7c132f5cb6d41d0a253029af5aa4d103c
Einar Ingebrigtsen <einar@dolittle.com>
2019-07-08 15:48:32 +0200

    Simplifying for Netlify

--- Source/Hugo/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker push dolittle/documentation
-kubectl patch deployment documentation --namespace dolittle -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
