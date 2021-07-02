3509ad974d5c66367f4b0d34dd58c2f6c4ed1718
Joel Hoisko <joel.hoisko@gmail.com>
2021-07-02 10:02:04 +0200

    Update docsy & fix sidebar scrolling and indentation

--- Source/static/js/scroll-sidebar-active-into-view.js
+++ Source/static/js/scroll-sidebar-active-into-view.js
@@ -1,5 +1,5 @@
 $(function () {
-    const activeSidebarItem = $('div.td-sidebar .td-sidebar-nav-active-item')[0];
+    const activeSidebarItem = $('div#td-sidebar-menu .td-sidebar-nav-active-item')[0];
     if (activeSidebarItem) {
         activeSidebarItem.scrollIntoView({ block: 'center' });
     }
